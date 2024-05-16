8109c78b03780b8dd90bda80dc0177d5c1e13e63
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2024-05-16 18:31:55 +0800

    feat: 调整异常定义使其能够被捕捉

--- tsugu_api_async/exception.py
+++ tsugu_api_async/exception.py
@@ -1,11 +1,11 @@
-class RoomSubmitFailure(BaseException):
+class RoomSubmitFailure(Exception):
     '''房间号上传失败'''
     def __init__(self, response: str) -> None:
         '''初始化'''
         self.message = response
         return
 
-class RoomQueryFailure(BaseException):
+class RoomQueryFailure(Exception):
     '''房间号获取失败'''
     def __init__(self, response: str) -> None:
         '''初始化'''
