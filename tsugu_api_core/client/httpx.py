e52a8bb728eed6d05c14306cd81c89adc56dc12d
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2025-04-05 02:22:47 +0800

    feat: 添加对高版本 `httpx` 已废除内容的适配

--- tsugu_api_core/client/httpx.py
+++ tsugu_api_core/client/httpx.py
@@ -105,13 +105,22 @@ class Client(_Client):
     
     @override
     async def arequest(self, request: Request) -> Response:
-        _request = httpx.Request(
-            request.method,
-            request.url,
-            params=request.params,
-            data=cast(dict, dumps(request.data)) if request.data is not None else request.data,
-            headers=request.headers,
-        )
+        if __HTTPX_ABOVE_0_28_0__:
+            _request = httpx.Request(
+                request.method,
+                request.url,
+                params=request.params,
+                content=cast(dict, dumps(request.data)) if request.data is not None else request.data,
+                headers=request.headers,
+            )
+        else:
+            _request = httpx.Request(
+                request.method,
+                request.url,
+                params=request.params,
+                data=cast(dict, dumps(request.data)) if request.data is not None else request.data,
+                headers=request.headers,
+            )
         
         _response = await self._async_client.send(_request)
         
