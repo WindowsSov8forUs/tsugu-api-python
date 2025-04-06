311f189f1d79f89bad613247cfad84a337f1cc76
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2025-04-06 15:42:41 +0800

    release: 1.5.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as readme:
 
 setup(
     name='tsugu-api-python',
-    version='1.5.5',
+    version='1.5.6',
     author='WindowsSov8',
     author_email='qwertyuiop2333@hotmail.com',
     description='Tsugu BanGDream Bot 的功能 API 统合包',
