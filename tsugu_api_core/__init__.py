b754d683554c2049515f58c2775a683a67f157cd
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2025-04-06 13:49:37 +0800

    feat: 修改 settings 定义方式

--- tsugu_api_core/__init__.py
+++ tsugu_api_core/__init__.py
@@ -3,8 +3,9 @@
 from . import utils as utils
 from . import client as client
 from . import _typing as _typing
-from . import settings as settings
 from . import _network as _network
 from . import exception as exception
 
 from .client import register_client as register_client
+
+from .settings import settings as settings
--- tsugu_api_core/settings.py
+++ tsugu_api_core/settings.py
@@ -8,68 +8,66 @@ class Client(str, Enum):
     AIO_HTTP = 'aiohttp'
     '''使用 `aiohttp`'''
 
-client: Client = Client.HTTPX
-'''使用的 HTTP 客户端'''
+class Settings:
+    '''tsugu_api 配置类'''
 
-timeout: float = 10
-'''请求超时时间'''
+    client: Client = Client.HTTPX
+    '''使用的 HTTP 客户端'''
 
-proxy: str = ''
-'''代理地址'''
+    timeout: float = 10
+    '''请求超时时间'''
 
-backend_url: str = 'http://tsugubot.com:8080'
-'''
-后端地址
+    proxy: str = ''
+    '''代理地址'''
 
-默认为 Tsugu 官方后端，若有自建后端服务器可进行修改。
-'''
-backend_proxy: bool = True
-'''
-是否使用后端代理
+    backend_url: str = 'http://tsugubot.com:8080'
+    '''
+    后端地址
 
-当设置代理地址后可修改此项以决定是否使用代理。
+    默认为 Tsugu 官方后端，若有自建后端服务器可进行修改。
+    '''
+    backend_proxy: bool = True
+    '''
+    是否使用后端代理
 
-默认为 True，即使用后端代理。若使用代理时后端服务器无法访问，可将此项设置为 False。
-'''
-userdata_backend_url: str = 'http://tsugubot.com:8080'
-'''
-用户数据后端地址
+    当设置代理地址后可修改此项以决定是否使用代理。
 
-所有的 `/user` 路由和 `/station` 路由都基于此
+    默认为 True，即使用后端代理。若使用代理时后端服务器无法访问，可将此项设置为 False。
+    '''
+    userdata_backend_url: str = 'http://tsugubot.com:8080'
+    '''
+    用户数据后端地址
 
-由于这些路由需要用户数据库的支持，部分 tsugu 后端可能不存在该路由
+    所有的 `/user` 路由和 `/station` 路由都基于此
 
-默认为 Tsugu 官方后端，若有自建后端服务器可进行修改。
-'''
-userdata_backend_proxy: bool = True
-'''
-是否使用用户数据后端代理
+    由于这些路由需要用户数据库的支持，部分 tsugu 后端可能不存在该路由
 
-当设置代理地址后可修改此项以决定是否使用代理。
+    默认为 Tsugu 官方后端，若有自建后端服务器可进行修改。
+    '''
+    userdata_backend_proxy: bool = True
+    '''
+    是否使用用户数据后端代理
 
-默认为 True，即使用后端代理。若使用代理时后端服务器无法访问，可将此项设置为 False。
-'''
+    当设置代理地址后可修改此项以决定是否使用代理。
 
-use_easy_bg: bool = True
-'''
-是否使用简易背景，使用可在降低背景质量的前提下加快响应速度。
+    默认为 True，即使用后端代理。若使用代理时后端服务器无法访问，可将此项设置为 False。
+    '''
 
-默认为 True，即使用简易背景。若不使用简易背景，可将此项设置为 False。
-'''
-compress: bool = True
-'''
-是否压缩返回数据，压缩可减少返回数据大小。
+    use_easy_bg: bool = True
+    '''
+    是否使用简易背景，使用可在降低背景质量的前提下加快响应速度。
 
-默认为 True，即压缩返回数据。若不压缩返回数据，可将此项设置为 False。
-'''
+    默认为 True，即使用简易背景。若不使用简易背景，可将此项设置为 False。
+    '''
+    compress: bool = True
+    '''
+    是否压缩返回数据，压缩可减少返回数据大小。
+
+    默认为 True，即压缩返回数据。若不压缩返回数据，可将此项设置为 False。
+    '''
+
+settings = Settings()
 
 __all__ = [
-    'timeout',
-    'proxy',
-    'backend_url',
-    'backend_proxy',
-    'userdata_backend_url',
-    'userdata_backend_proxy',
-    'use_easy_bg',
-    'compress'
+    'settings',
 ]
